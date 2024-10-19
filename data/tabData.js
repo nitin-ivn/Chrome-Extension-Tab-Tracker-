d1a06fe19c81407c868070146d739d8411ed61fd
nitin-ivn <ivnsainitin@gmail.com>
2024-10-19 19:53:30 +0530

    changed some styling

--- data/tabData.js
+++ data/tabData.js
@@ -1,27 +1,31 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tabData = void 0;
-exports.insertData = insertData;
-exports.MatchingTab = MatchingTab;
-var TabData = /** @class */ (function () {
-    function TabData(tabID, name, url, openedAt) {
-        this.tabID = tabID;
-        this.name = name;
-        this.url = url;
-        this.openedAt = openedAt;
-        this.instances = 1;
-    }
-    return TabData;
-}());
-exports.tabData = new Map();
-function insertData(tabId, name, url, openedAt) {
-    if (exports.tabData.has(tabId)) {
-        exports.tabData.get(tabId).instances += 1;
-    }
-    else {
-        exports.tabData.set(tabId, new TabData(tabId, name, url, openedAt));
+class TabData{
+    tabID
+    name
+    url
+    openedAt
+    closedAt
+    timeSpent
+
+    instances = 1;
+
+    constructor(tabID,name,url,openedAt){
+        this.tabID =tabID
+        this.name = name,
+        this.url = url,
+        this.openedAt = openedAt
     }
 }
-function MatchingTab(tabId) {
-    return exports.tabData.get(tabId);
+
+export let tabData = new Map();
+
+export function insertData(tabId,name,url,openedAt){
+     if(tabData.has(tabId)){
+        tabData.get(tabId).instances += 1;
+     }else{
+        tabData.set(tabId, new TabData(tabId, name, url, openedAt)); 
+     } 
+}
+
+export function MatchingTab(tabId){
+    return tabData.get(tabId);
 }
